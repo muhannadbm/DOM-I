87c30817fa506cb8eb16afd30aebee9458b8c7f5
muhannadbm <mhd31145@hotmail.com>
2021-07-06 00:00:46 -0400

    final changes, converted js file to use siteContent info plus some minor changes

--- js/index.js
+++ js/index.js
@@ -60,16 +60,16 @@ const features = mynav.querySelector('a:nth-of-type(4)');
 const about = mynav.querySelector('a:nth-of-type(5)');
 const contact = mynav.querySelector('a:nth-of-type(6)');
 
-services.textContent = 'Services'
-product.textContent = 'Product'
-vision.textContent = 'Vision'
-features.textContent = 'Features'
-about.textContent = 'About'
-contact.textContent = 'Contact'
+services.textContent = siteContent["nav"]["nav-item-1"]
+product.textContent = siteContent["nav"]["nav-item-2"]
+vision.textContent = siteContent["nav"]["nav-item-3"]
+features.textContent = siteContent["nav"]["nav-item-4"]
+about.textContent = siteContent["nav"]["nav-item-5"]
+contact.textContent = siteContent["nav"]["nav-item-6"]
 
 const mydiv = document.querySelector('.cta')
 const codeimg = mydiv.querySelector('#cta-img')
-codeimg.src = 'img/header-img.png'
+codeimg.src = siteContent["cta"]["img-src"]
 
 const header = document.querySelector('.cta-text h1')
 header.textContent = 'DOM'
@@ -92,16 +92,15 @@ mybutton.textContent = 'Get Started'
 
 
 const featuresheader = document.querySelector('.text-content h4')
-featuresheader.textContent = 'Features'
+featuresheader.textContent = siteContent["main-content"]["features-h4"]
 const featuresp = document.querySelector('.text-content p')
-featuresp.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
-
+featuresp.textContent = siteContent["main-content"]["features-content"]
 
 const textconten1 = document.querySelector('.text-content:nth-of-type(2)')
 const myheader1 = textconten1.querySelector('h4');
-myheader1.textContent = 'About'
+myheader1.textContent = siteContent["main-content"]["about-h4"]
 const myp1 = textconten1.querySelector('p');
-myp1.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+myp1.textContent = siteContent["main-content"]["about-content"]
 
 
 
@@ -109,42 +108,37 @@ myp1.textContent = 'About content elementum magna eros, ac posuere elvit tempus 
 const bottom = document.querySelector('.bottom-content')
 
 const servicesheader = bottom.querySelector('.text-content:nth-of-type(1) h4');
-servicesheader.textContent = 'Services'
+servicesheader.textContent = siteContent["main-content"]["services-h4"]
 const servicesp = servicesheader.nextElementSibling;
-servicesp.textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+servicesp.textContent = siteContent["main-content"]["services-content"]
 
 
 const Productheader = bottom.querySelector('.text-content:nth-of-type(2) h4');
-Productheader.textContent = 'Product'
+Productheader.textContent = siteContent["main-content"]["product-h4"]
 const Productp = Productheader.nextElementSibling;
-Productp.textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+Productp.textContent = siteContent["main-content"]["product-content"]
 
 
 const visionheader = bottom.querySelector('.text-content:nth-of-type(3) h4');
-visionheader.textContent = 'Vision'
+visionheader.textContent = siteContent["main-content"]["vision-h4"]
 const visionp = visionheader.nextElementSibling;
-visionp.textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+visionp.textContent = siteContent["main-content"]["vision-content"]
 
 
 
 const midimage = document.querySelector('#middle-img')
-midimage.src = 'img/mid-page-accent.jpg'
+midimage.src = siteContent["main-content"]["middle-img-src"]
 
 const mycontact = document.querySelector('.contact')
 
-mycontact.querySelector('h4').textContent = 'Contact'
-mycontact.querySelector('p:nth-of-type(1)').textContent = '123 Way 456 Street'
-var mybr = document.createElement('br');
-mycontact.querySelector('p:nth-of-type(1)').appendChild(mybr);
+mycontact.querySelector('h4').textContent = siteContent["contact"]["contact-h4"]
+mycontact.querySelector('p:nth-of-type(1)').textContent = siteContent["contact"]["address"]
 
-var t = document.createTextNode("Somewhere, USA");
-mycontact.querySelector('p:nth-of-type(1)').appendChild(t);
+mycontact.querySelector('p:nth-of-type(2)').textContent = siteContent["contact"]["phone"]
 
-mycontact.querySelector('p:nth-of-type(2)').textContent = '1 (888) 888-8888'
-
-mycontact.querySelector('p:nth-of-type(3)').textContent = 'sales@greatidea.io'
+mycontact.querySelector('p:nth-of-type(3)').textContent = siteContent["contact"]["email"]
 
 const copy = document.querySelector('footer p')
-copy.textContent = 'Copyright Great Idea! 2018'
+copy.textContent = siteContent["footer"]["copyright"]
 
-
+mycontact.querySelector('p:nth-of-type(1)').style.width = '22%'
